739db3587b237d01861618270302f2fb9b2fd15f
voidddd7 <726888@student.dusd.net>
2024-10-03 11:08:34 -0700

    2nd scripting assignment
    completed and finished by second scripting assignment

---
+++ ScriptingProjectP4Garcia/Assets/Scripts/VariablesAndFunctions.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    //Create a variable of type int
+    int myInt = 7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}
