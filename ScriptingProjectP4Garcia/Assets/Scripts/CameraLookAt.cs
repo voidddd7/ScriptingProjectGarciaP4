b7e3cf32eb13ee28dd04952df5a144463fb96ba8
voidddd7 <726888@student.dusd.net>
2025-01-22 10:29:16 -0800

    LookAt
    i finished my script

---
+++ ScriptingProjectP4Garcia/Assets/Scripts/CameraLookAt.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLookAt : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(target);
+    }
+}
