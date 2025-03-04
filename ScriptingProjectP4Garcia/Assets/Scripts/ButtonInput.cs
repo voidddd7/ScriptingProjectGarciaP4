23a0615479fc1496b7db98a13c382d33fdfa80fa
voidddd7 <726888@student.dusd.net>
2025-03-04 10:49:22 -0800

    new scripts
    finished scripts

---
+++ ScriptingProjectP4Garcia/Assets/Scripts/ButtonInput.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonInput : MonoBehaviour
+{
+    public Image graphic;
+    public Sprite standard;
+    public Sprite downgfx;
+    public Sprite upgfx;  
+    public Sprite heldgfx;
+    public Text boolDisplay1;
+    public Text boolDisplay2;
+    public Text boolDisplay3;
+ 
+    void Start()
+    {
+        graphic.sprite = standard;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool down = Input.GetButtonDown("Jump");
+        bool held = Input.GetButtonDown("Jump");
+        bool up = Input.GetButtonDown("Jump");
+
+        if (down)
+        {
+            graphic.sprite = downgfx;
+        }
+        else if (held)
+        {
+            graphic.sprite = heldgfx;
+        }
+        else if (up)
+        {
+            graphic.sprite = upgfx;
+        }
+        else
+        {
+            graphic.sprite = standard;
+        }
+
+        boolDisplay1.text = " " + down;
+        boolDisplay2.text = " " + held;
+        boolDisplay3.text = " " + held;
+    }
+}
