eb2fb37dfcf82bfaa36000aede6a889d1cc72a77
voidddd7 <726888@student.dusd.net>
2025-03-28 11:40:41 -0700

    data type
    finished data type scripts

---
+++ ScriptingProjectP4Garcia/Assets/Scripts/DatatypeScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatatypeScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Value type variable
+        Vector3 pos = transform.position;
+        pos = new Vector3(0, 2, 0);
+
+        //Reference type variable
+        Transform tran = transform;
+        tran.position = new Vector3(0, 2, 0);
+    }
+
+   
+}
